be96424e97432fc72683373390571890b7de17d6
Александр Занин <bsalexandrzanin@gmail.com>
2024-08-20 19:26:48 +0300

    remove dir

--- handlers/customHandle.py
+++
@@ -1,8 +0,0 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-from keyboards import key
-from config import bot
-
-
-@bot.message_handler(commands=["create_markup"])
-
-
--- keyboards/key.py
+++
@@ -1,7 +0,0 @@
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def gen_markup():
-    # Создаем объекты кнопок
-    
